249640b7f8b0da3d493af185826e61fe921062c9
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2021-09-26 23:11:39 +0300

    set up environment, install pandas, set up git ignore

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+
+
+
+def main():
+    """
+    The central code of this application. Binds modules together, 
+    checks the input variables, creates necessary directories, 
+    cleans up mess and etc.
+    """
