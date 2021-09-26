eb782cc1c51881bec9a8e0d1b225fd25ff489579
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2021-09-26 23:31:00 +0300

    Check for existence of a file, before checking its format

--- app/file_handlers.py
+++ app/file_handlers.py
@@ -8,9 +8,9 @@ from .errors import NotTableFileError
 
 def file_checker(file_location: str, filename: str):
     #checks whether it is an excel table (or csv) and its existence
-    if not filename.endswith(('.xls', '.xlsx', '.csv')):
-        # if it is not an excel file, raise NotTableFileError
-        raise NotTableFileError(filename)
     if not os.path.exists( os.path.join(file_location, filename) ):
         #If there is no such file, raise FileNotFoundError
         raise FileNotFoundError
+    if not filename.endswith(('.xls', '.xlsx', '.csv')):
+        # if it is not an excel file, raise NotTableFileError
+        raise NotTableFileError(filename)
