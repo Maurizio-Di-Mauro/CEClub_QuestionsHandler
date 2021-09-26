b9ff934a38de3f7411e63b39e8023ffa72c7e919
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2021-09-26 23:28:46 +0300

    Add filename checker

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 import os
 import pandas as pd
 
+#my imports
+import app.file_handlers
 
 
 
-def main():
+
+def main(filename: str):
     """
     The central code of this application. Binds modules together, 
     checks the input variables, creates necessary directories, 
