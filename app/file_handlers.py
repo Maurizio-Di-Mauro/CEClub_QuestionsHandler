6b313593fabefaa8e9c28b7b629cab29bb5cf3c0
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2021-09-29 08:27:05 +0300

    give better name to functions

--- app/file_handlers.py
+++ app/file_handlers.py
@@ -6,7 +6,7 @@ from .errors import NotTableFileError
 
 
 
-def file_checker(file_location: str, filename: str):
+def file_validation(file_location: str, filename: str):
     #checks whether it is an excel table (or csv) and its existence
     if not os.path.exists( os.path.join(file_location, filename) ):
         #If there is no such file, raise FileNotFoundError
