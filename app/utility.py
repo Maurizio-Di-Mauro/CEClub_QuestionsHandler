b8b4849e14ea92b3a05d47abeac3a4f0f317a7ea
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2021-11-21 16:58:29 +0300

    Change func name to get the last sunday

--- app/utility.py
+++ app/utility.py
@@ -3,9 +3,9 @@ from datetime import date, datetime, timedelta
 
 
 
-def get_previous_sunday() -> datetime:
+def get_last_sunday() -> datetime:
     today = date.today()
-    today = datetime(today.year, today.month, today.day, 13, 30, 00)
+    today = datetime(today.year, today.month, today.day, 12, 00, 00)
     index: int = (today.weekday() + 1) % 7
     this_sunday: datetime = today - timedelta(days = index)
     return this_sunday
