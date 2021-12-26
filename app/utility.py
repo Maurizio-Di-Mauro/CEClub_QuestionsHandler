f89e5b6fc1f92c6f47ba9f06ab95d4a9ea1c7094
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2021-12-26 12:41:22 +0300

    correctly calculate last sunday

--- app/utility.py
+++ app/utility.py
@@ -5,15 +5,15 @@ from datetime import date, datetime, timedelta
 
 def get_last_sunday() -> datetime:
     today = date.today()
-    today = datetime(today.year, today.month, today.day, 12, 00, 00)
-    index: int = (today.weekday() + 1) % 7
+    today = datetime(today.year, today.month, today.day, 13, 00, 00)
+    index: int = today.weekday() + 1
     last_sunday: datetime = today - timedelta(days = index)
     return last_sunday
 
 
 def get_current_sunday() -> datetime:
     today = date.today()
-    today = datetime(today.year, today.month, today.day, 12, 00, 00)
+    today = datetime(today.year, today.month, today.day, 13, 00, 00)
     index: int = 7 - (today.weekday() + 1)
     this_sunday: datetime = today + timedelta(days = index)
     return this_sunday
